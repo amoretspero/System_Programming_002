bd25c86c7d0d166650ff092e48b3792a755647db
amoretspero <amoretspero@gmail.com>
2015-06-11 10:49:02 -0700

    Proxy_Lab - thread part doing.

--- Proxy_Lab/proxylab/proxy.c
+++ Proxy_Lab/proxylab/proxy.c
@@ -26,6 +26,8 @@ ssize_t Rio_readn_w(int fd, void *ptr, size_t nbytes);
 ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen);
 void Rio_writen_w(int fd, void *usrbuf, size_t n);
 
+void* thread(void* vargp);
+
 char* get_host(char* str)
 {
 	char* host = strtok(str, " ");
@@ -59,23 +61,25 @@ int main(int argc, char **argv)
     }
 
 		int clientfd, serverfd;
-		int port_client;
+		int port_proxy;
 		int port_server;
 		char* host;
 		char buf[MAXLINE];
 		rio_t rio;
 
-		port_client = atoi(argv[1]);
+		port_proxy = atoi(argv[1]);
 
 		struct sockaddr_in clientaddr;
 		int clientlen = sizeof(clientaddr);
-		int listenfd = Open_listenfd(port_client);
+		int listenfd = Open_listenfd(port_proxy);
 
 		int proxy_fd;
 
+		pthread_t tid;
+
 		while(1)
 		{
-			int* connfdp = Malloc(sizeof(int));
+			/*int* connfdp = Malloc(sizeof(int));
 			struct hostent* hp;
 			char* haddrp;
 			int client_port;
@@ -106,7 +110,11 @@ int main(int argc, char **argv)
 			Rio_readlineb(&rio, msg, MAXLINE);
 			printf("Proxy received from server : %s\n", msg);
 
-			// Retrun to client. 
+			// Retrun to client. */
+			int* connfd = (int*)malloc(sizeof(int));
+			*connfd = Accept(listenfd, (SA*)&clientaddr, &clientlen);
+			Pthread_create(&tid, NULL, thread, connfd);
+
 		}
 
 
@@ -114,10 +122,83 @@ int main(int argc, char **argv)
     exit(0);
 }
 
+void* thread(void* vargp)
+{
+	Pthread_detach(pthread_self());
+	
+	int connfd = *((int*)vargp);
+
+	Free(vargp);
+
+	char buf[MAXLINE];
+	Rio_readn_w(connfd, buf, MAXLINE);
+
+	char* host; int port_server; char* msg; 
+	rio_t rio;
+	host = get_host(buf);
+	port_server = atoi(get_port(buf));
+	msg = get_message(buf);
+	char* msg_received = (char*)malloc(sizeof(char) * strlen(msg));
+
+	int proxy_fd = Open_clientfd(host, port_server);
+	Rio_readinitb(&rio, proxy_fd);
+	
+	Rio_writen_w(proxy_fd, msg, strlen(buf));
+	Rio_readlineb_w(&rio, msg_received, MAXLINE);
+
+	close(proxy_fd);
+
+	Rio_writen_w(connfd, msg_received, strlen(msg_received));
+
+	Close(connfd);
+
+	return NULL;
+}
+
+
 
 int open_clientfd_ts(char* hostname, int port, sem_t* mutexp)
 {
-	return 0;
+	int clientfd;
+	int len = 16;
+	int ghn_res = -1;
+	int err;
+	struct hostent* hp;
+	struct hostent hbuf;
+	struct sockaddr_in serveraddr;
+	char* buf = (char*)malloc(sizeof(char) * len);
+
+	if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		return -1;
+	}
+
+	while ((ghn_res = gethostbyname_r(hostname, &hbuf, buf, len, &hp, &err)) == ERANGE)
+	{
+		len = len * 2;
+		buf = realloc(buf, len);
+		if (NULL == buf)
+		{
+			perror("realloc");
+		}
+	}
+
+	if (hp == NULL)
+	{
+		return -2;
+	}
+
+	bzero((char*)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	bcopy((char*)hp->h_addr_list[0], (char*)&serveraddr.sin_addr.s_addr, hp->h_length);
+	serveraddr.sin_port = htons(port);
+
+	if (connect(clientfd, (SA*)&serveraddr, sizeof(serveraddr)) < 0)
+	{
+		return -1;
+	}
+
+	return clientfd;
 }
 
 ssize_t Rio_readn_w(int fd, void* ptr, size_t nbytes)
